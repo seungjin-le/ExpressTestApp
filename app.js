a6d9a1ff52a43f265a73479918c115dcd1139259
Lee Seung Jin <dltmdwls154@gmail.com>
2023-10-17 00:13:44 +0900

    setting

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 router.get('/', async (req, res) => {
-	res.send('Get');
+	res.send('<h1>Hello world</h1>');
 });
 
 router.param('id', (req, res, next, value) => {
 	console.log(value, 'qweqweqew');
-	req.params.id = value + 'zxcvzxcvzxcv12321321123312';
+	req.params.id = value + '12321321123312';
 	next();
 });
 
